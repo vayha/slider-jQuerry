a7f91c03a3cec8d9278c7488dcbd400ae114a4f1
Mateusz Wegrzyn <matiice@interia.pl>
2017-04-08 16:58:21 +0200

    next

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+$(function(){
+    
+    'use strict'
+
+
+
+
+var slider = $('#slider');
+var slideshow = $('.slide-show');
+var slideCount = $('.single-slide').length;
+var slideWidth = 100 / slideCount;
+var slideIndex = 0;
+
+
+
+    console.log(slideCount);
+    
+
+});
+    
+
+
+//$('h1').hide(5000).show(3000);
+//    //pokaz ukryj animacja
+//    
+//    
+//$('h2').fadeOut(5000).fadeIn(3000);
+
+//slide();    
+//function slide(){
+//    $('h3').slideUp(5000).slideDown(3000, slide);
+//}
+
+//    var rand = Math.floor((Math.random() * 100));
+//    
+//$('h3').animate({'font-size' : '1em', 'margin-left' : rand + 'px'}, 2000, parBack);
+//    
+//    function parBack() {
+//        $('p').css('background', 'red');
+//    }
+//    
